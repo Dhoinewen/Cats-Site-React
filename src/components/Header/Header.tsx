79aae7ade1f2172d3629dec7875cf984757d6391
Maxim <pogchamp1401@gmail.com>
2023-03-12 14:56:05 +0200

    header init

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+type HeaderProps = unknown;
+
+const Header: FC<HeaderProps> = () => {
+  return (
+    <div>
+      <h1>Aboba</h1>
+    </div>
+  );
+};
+
+export { Header };
