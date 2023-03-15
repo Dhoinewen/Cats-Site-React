cde7bcd86cd4a0ff574bd51a118134cc390ffe6f
Maxim <pogchamp1401@gmail.com>
2023-03-15 22:00:30 +0200

    changes in styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ type HeaderProps = unknown;
 
 const Header: FC<HeaderProps> = () => {
   return (
-    <div>
-      <h1>Aboba</h1>
+    <div className="mx-auto w-1/2 h-8 bg-black text-blue-700 ">
+      <h1 className="flex items-center justify-center">Aboba</h1>
     </div>
   );
 };
